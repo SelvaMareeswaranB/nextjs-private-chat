1b30acdd1dd192d35daf8a06c9a865ed76167a71
Selva Mareeswaran <selvamareeswaranb@gmail.com>
2025-12-31 18:48:45 +0530

    fix(elysia): fix elysia client configuration-1

--- lib/client.ts
+++ lib/client.ts
@@ -3,4 +3,4 @@ import type { App } from '../app/api/[[...slugs]]/route'
 
 // this require .api to enter /api prefix
 const url = process.env.NEXT_PUBLIC_BACKEND_URL ?? "http://localhost:3000";
-export const elysia = treaty(url);
+export const client = treaty<App>(url).api
