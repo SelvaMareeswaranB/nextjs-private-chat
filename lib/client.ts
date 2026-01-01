1e791b1709fb8fad0cf0e361019414ff6b48f23b
Selva Mareeswaran <selvamareeswaranb@gmail.com>
2026-01-01 09:22:29 +0530

    fix(elysia-client): fix backend URL

--- lib/client.ts
+++ lib/client.ts
@@ -1,12 +1,14 @@
-import { treaty } from '@elysiajs/eden'
-import type { App } from '../app/api/[[...slugs]]/route'
+import { treaty } from '@elysiajs/eden';
+import type { App } from '../app/api/[[...slugs]]/route';
 
-// this require .api to enter /api prefix
-const url =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : process.env.NEXT_PUBLIC_BACKEND_URL;
+// pick the right URL
+const url = process.env.NEXT_PUBLIC_BACKEND_URL;
 
-if (!url) throw new Error("NEXT_PUBLIC_BACKEND_URL is not defined");
+if (!url) {
+  throw new Error(
+    "NEXT_PUBLIC_BACKEND_URL is not defined. Check your environment variables."
+  );
+}
 
-export const client = treaty<App>(url).api
+// create Treaty client
+export const client = treaty<App>(url).api;
