1f9871ca4ab6c2167501280801754efef730ee55
Selva Mareeswaran <selvamareeswaranb@gmail.com>
2026-01-01 09:14:10 +0530

    fix(elysia-client): use HTTPS backend URL to prevent mixed content

--- lib/client.ts
+++ lib/client.ts
@@ -2,5 +2,11 @@ import { treaty } from '@elysiajs/eden'
 import type { App } from '../app/api/[[...slugs]]/route'
 
 // this require .api to enter /api prefix
-const url =process.env.NODE_ENV === "development" ? "http://localhost:3000"  :  process.env.NEXT_PUBLIC_BACKEND_URL!;
+const url =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : process.env.NEXT_PUBLIC_BACKEND_URL;
+
+if (!url) throw new Error("NEXT_PUBLIC_BACKEND_URL is not defined");
+
 export const client = treaty<App>(url).api
